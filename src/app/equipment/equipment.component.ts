571ea96a5ea0a4cf3e708ea8836fa52a51a020e8
Joshua Lynch <joshua.lynch@hotmail.com>
2022-04-11 22:48:26 -0400

    added styling to equipment && added component to app

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = ['Habitat Dome', 'Drones', 'Food Containers', 'Oxygen Tanks']
+  
   constructor() { }
 
   ngOnInit(): void {
